cf946fec0e42af4ff9d240d95f7c1db8626e3c1a
nick <nlitzc7@gmail.com>
2020-09-03 12:16:11 -0700

    fixed rendering home unauthed

--- tacktivity/client/src/components/Login.js
+++ tacktivity/client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { login, demoLogin } from '../store/auth';
+import { login } from '../store/auth';
 import { Redirect } from 'react-router-dom';
 
 function Login({ showModal }) {
