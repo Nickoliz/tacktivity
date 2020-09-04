ed973703af4c90ed83818ebabc3d0a786c2ed16a
nick <nlitzc7@gmail.com>
2020-09-04 13:54:10 -0700

    trying .then()

--- tacktivity/routes/api/photos.js
+++ tacktivity/routes/api/photos.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const asyncHandler = requrie('express-async-handler');
+const asyncHandler = require('express-async-handler');
 const Unsplash = require('unsplash-js');
 const toJson = require('unsplash-js').toJson;
 
@@ -7,16 +7,16 @@ const unsplash = new Unsplash({ accessKey: process.env.UNSPLASH_API_KEY });
 const router = express.Router();
 
 // GET /photos
-router.get("/", asyncHandler(async function(req, res ,next) {
-  // unsplash.photos.listPhotos(1, 20, "latest")
-  //   .then(toJson)
-  //   .then(json => {
+router.get("/", asyncHandler(async function (req, res, next) {
+  unsplash.photos.listPhotos(1, 20, "latest")
+    .then(toJson)
+    .then(json => {
+      console.log(json);
+    })
 
-  //   })
-
-    const res = await unsplash.photos.listPhotos(1, 20);
-    const data = await res.toJson();
-    console.log(data.urls);
+  // const res = await unsplash.photos.listPhotos(1, 20);
+  // const data = await res.toJson();
+  // console.log(data.urls);
 
 }))
 
