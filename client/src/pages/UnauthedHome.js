8cc04468a8342d39a0b5b8cde66f3deda3028f6f
nick <nlitzc7@gmail.com>
2020-12-01 14:49:24 -0800

    updated landing page scroll lock

--- client/src/pages/UnauthedHome.js
+++ client/src/pages/UnauthedHome.js
@@ -3,6 +3,8 @@ import Unauthed from '../components/Unauthed';
 import UnauthMosaic from '../components/UnauthMosaic';
 
 function UnauthedHome() {
+  const body = document.getElementById('body-id');
+  body.classList.add('lock-scroll');
 
 
   return (
