714e8d25f8200c07931bda754d4f18e8854738fa
nick <nlitzc7@gmail.com>
2020-12-08 12:19:34 -0800

    updated Dashboard layout and added Dashboard CSS

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Authed from '../components/Authed';
+import '../../src/css/userdashboard.css';
 
 export default function UserDashboard() {
   const user = useSelector(state => state.auth);
-
-  console.log(user)
 
   return (
     <>
       <Authed />
       <div className='user-information'>
         <div id='user-photo'>
-          {/* <img src={user} /> */}
+          <i className='fas fa-user-circle fa-10x' />
         </div>
         <div id='user-real-name'>
-
+          {user.firstName, user.lastName}
         </div>
         <div id='username'>
 
